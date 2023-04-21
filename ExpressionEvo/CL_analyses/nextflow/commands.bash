330f82d91e0c52cb8c81c92757b985b1744ef5cd
petedprice <petedprice@gmail.com>
2023-04-21 10:27:53 +0100

    m

--- ExpressionEvo/CL_analyses/nextflow/commands.bash
+++ ExpressionEvo/CL_analyses/nextflow/commands.bash
@@ -1,1 +1,10 @@
-~/software/nextflow main.nf -resume
+~/software/nextflow run /home/bop20pp/software/supergene_wogtf/nextflow/genotyping_main.nf \
+	--reads reads \
+	--adapter adapters.fa \
+	--ref GCF_003957565.2_bTaeGut1.4.pri_genomic.fna \
+	--ref_index GCF_003957565.2_bTaeGut1.4.pri_genomic.fna.fai \
+	--cdna GCF_003957565.2_bTaeGut1.4.pri_genomic.cdna \
+	--metadata full_metadata.csv \
+	--contigs csvs/Z.csv \
+	-resume \
+	-bg
